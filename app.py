24bf7d0e67c0895c2735ff981b9496f0148488e8
Walter <wsoares@live.com>
2025-04-03 10:08:28 -0300

    criação teste

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Deploy automático funcionando! Código atualizado com sucesso."
+
+if __name__ == '__main__':
+    # Inicia o servidor Flask escutando em todas as interfaces na porta 5000
+    app.run(host='0.0.0.0', port=5000)
